c30dd3dc176cb55e97ae3edea081957a92ec8edf
lyg0114 <whdnseowkd@naver.com>
2020-09-07 21:01:35 +0900

    init

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,32 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter @Setter
+@ToString(of = {"id","name"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name="team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name){
+        this.name = name;
+    }
+
+
+
+
+}
